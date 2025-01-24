212b04fe8cc3e9a1df85f618734b6201b667f653
vudzijena.m@northeastern.edu <vudzijena.m@northeastern.edu>
2025-01-24 14:18:43 -0500

    Added Lab1_HelloWorld

---
+++ lab1_HelloWorld/src/lab1_helloworld/Lab1_HelloWorld.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lab1_helloworld;
+
+/**
+ *
+ * @author User
+ */
+public class Lab1_HelloWorld {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        System.out.println("This is Java");
+    }
+    
+}
